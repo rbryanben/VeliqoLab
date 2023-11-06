410bfceb2082b8b17703c6902042a7d7ebfc5721
rbryanben <rbryanben@gmail.com>
2023-11-06 09:06:17 +0200

    Custom Toast - Force dark mode

--- app/src/main/java/com/wapazock/veliqolab/views/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/wapazock/veliqolab/views/welcome/WelcomeActivity.kt
@@ -1,9 +1,12 @@
 package com.wapazock.veliqolab.views.welcome
 
+import android.app.Activity
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import com.wapazock.veliqolab.R
 import com.wapazock.veliqolab.views.login.loginActivity
 import com.wapazock.veliqolab.views.register.register
@@ -18,6 +21,9 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        // Force dark mode
+        forceDarkMode(this)
 
         // Assign view variables
         signInLink = this.findViewById(R.id.activityWelcomeSignInTextView)
@@ -39,5 +45,10 @@ class WelcomeActivity : AppCompatActivity() {
             val intent = Intent(this,ResetPasswordActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun forceDarkMode(activity: Activity) {
+        val uiMode: Int = activity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
